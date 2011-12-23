7df476c5acbf1425e3b8379a2d1cf1c87eef5430
Cagdas Bozman <bozman.cagdas@gmail.com>
2011-12-23 17:54:52 +0100

    add lesson 2

--- lessons/lesson2/step2/step.ml
+++ lessons/lesson2/step2/step.ml
@@ -1,3 +1,3 @@
-let good_output_start = "val y : int = 43" in
+let good_output_start = " : int =" in
 fun input output ->
   String.sub output 0 (String.length good_output_start) = good_output_start
