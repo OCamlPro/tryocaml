a681b74306b6d40f97e32c36aed6e4ddc06a1f2a
Fabrice Le Fessant <fabrice@ocamlpro.com>
2012-01-09 22:15:30 +0100

    Updated toplevel

--- tryocaml/ocaml-num/test.ml
+++ tryocaml/ocaml-num/test.ml
@@ -5,7 +5,7 @@ open Big
 
 let _ =
   let t = ref (int_of_int 1_000) in
-  for i = 0 to 10000 do
+  for i = 0 to 1000 do
     t := !t + !t + !t + !t;
     Format.fprintf ppf "------------------------------------------------";
     print ppf !t;
