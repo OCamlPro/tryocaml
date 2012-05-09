6de55a7a51a56513a97a13088ebbab83bfd14f4c
Çagdas Bozman <bozman.cagdas@gmail.com>
2012-05-09 15:42:05 +0200

    Updated examples and add Makefile in examples/

--- tryocaml/examples/moving_box.ml
+++ tryocaml/examples/moving_box.ml
@@ -1,16 +1,7 @@
-let document = Dom_html.document
-let window = Dom_html.window
+open Utils
 
-let _s s = Js.string s
-let _f f = Js.wrap_callback f
-
-let getElementById id =
-  Js.Opt.get (document##getElementById (_s id)) (fun () -> assert false)
-
-(***************************************)
-
-let canvas = Dom_html.createCanvas document
-let body = getElementById "main-title"
+let canvas = Dom_html.createCanvas doc
+let body = get_element_by_id "main-title"
 let context = canvas##getContext (Dom_html._2d_)
 
 let x = ref 10.
@@ -22,10 +13,10 @@ let redraw _ =
   y := if !y > 150. then 0. else !y +. 1.;
   context##fillStyle <- Js.string "red";
   context##fillRect (!x,!y, 20., 20.)
-
+    
 let _ =
   Dom.appendChild body canvas;
-  let interval_id = window##setInterval (_f redraw, 2.5) in
+  let _ = window##setInterval (_f redraw, 2.5) in
   ()
 
 
