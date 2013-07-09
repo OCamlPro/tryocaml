ba20c1712d5648eea36344f07da75ab0c0d7dce3
Çagdas Bozman <cagdas.bozman@ocamlpro.com>
2013-07-09 13:32:32 +0200

    move ocp-jslib in another repository

--- ocp-jslib/button.mli
+++
@@ -1,5 +0,0 @@
-val create : string -> (unit -> 'a) -> Dom_html.buttonElement Js.t
-
-val create_with_image : string -> int -> string -> (unit -> 'a) -> Dom_html.buttonElement Js.t
-
-val registered_buttons : (string * string) list ref
